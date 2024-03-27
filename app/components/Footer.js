c38e014f327538cf00b8c45f24f6e81223f598cc
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2024-03-27 08:36:28 -0700

    saving

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,24 +1,29 @@
-import React from "react";
-import Image from "next/image";
+// import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Context } from "../context/appContext";
 import styles from "../styles/footer.css";
-import EmailList from "./EmailList";
 
-const Footer = (isLargeScreen) => (
-  <footer className="footer">
-    {/* <div className="larger-footer-div"> */}
-    <div className="all-rights">
-      {/* <div className="foot-cont"> */}
-      <p className="foot">© 2024 Center for Cinematic and Experimental Arts</p>
-      <p className="foot">All Rights Reserved</p>
-    </div>
-    {/* </div> */}
-    {/* <div className="socials">
-        <i className="fa-brands fa-instagram" />
-        <i className="fa-brands fa-twitter" />
-        <i className="fa-brands fa-facebook-f" />
-      </div> */}
-    {/* </div> */}
-  </footer>
-);
+const Footer = () => {
+  const { store, actions } = useContext(Context);
+  console.log("INS?", store.isNavOpen);
+  const [isFooter, setIsFooter] = useState(false);
+  console.log("FOOT?", isFooter);
+
+  useEffect(() => {
+    setIsFooter(!store.isNavOpen);
+  }, [store.isNavOpen]);
+
+  return (
+    <footer className="footer">
+      {isFooter && (
+        <div className="all-rights">
+          <p className="foot">
+            2024 © Center for Cinematic and Experimental Arts
+          </p>
+        </div>
+      )}
+    </footer>
+  );
+};
 
 export default Footer;
