a66798da704e0a2de54846a25c34ac934cdf6121
Mara <maracodes213@gmail.com>
2024-08-02 18:29:29 -0500

    css, footer and about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./about.css";
-import Image from "next/image";
 import FakeNavBar from "../components/FakeNavbar";
 import Footer from "../components/Footer";
 
 const About = () => {
   return (
     <>
-      <FakeNavBar />
       <div className="about-page">
+        <FakeNavBar />
         <div className="about-div">
           <span className="">
             The Center for Cinematic and Experimental Arts is a nonprofit
